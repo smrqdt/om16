2212382cc42a091a3d86c712f856b09a2c0aa82b
euleule <rokr42@gmail.com>
2013-07-20 12:35:19 +0200

    Removed librarys. Added composer to load dependencys. fixed some weird user session behavior.

---
+++ app/auth/AuthProvider.php
@@ -0,0 +1,65 @@
+<?php
+namespace Strong\Provider;
+class AuthProvider extends \Strong\Provider {
+
+	/**
+	 * User login check based on provider
+	 *
+	 * @return boolean
+	 */
+	public function loggedIn() {
+		return (isset($_SESSION['auth_user']) && !empty($_SESSION['auth_user']));
+	}
+
+	/**
+	 * To authenticate user based on username
+	 * and password
+	 *
+	 * @param string $username
+	 * @param string $password
+	 * @return boolean
+	 */
+	public function login($username, $password) {
+		if(! is_object($username)) {
+			$user = \User::find_by_username($username);
+		}
+		if($user == null){
+			return false;
+		}
+
+		if(($user->username === $username) && $this->verify($user, $password)) {
+			return $this->completeLogin($user);
+		}
+
+		return false;
+	}
+
+	private function verify($user, $password){
+		$bcrypt = new Bcrypt();
+		return $bcrypt->verify($password, $user->password);
+	}
+
+	public function initPassword($password){
+		$bcrypt = new Bcrypt();
+		return $bcrypt->hash($password);
+	}
+
+	/**
+	 * Login and store user details in Session
+	 *
+	 * @param array $user
+	 * @return boolean
+	 */
+	protected function completeLogin($user) {
+		$users = \User::find($user->id);
+		$users->save();
+
+		$userInfo = array(
+				'id' => $user->id,
+				'username' => $user->username,
+				'logged_in' => true
+		);
+
+		return parent::completeLogin($userInfo);
+	}
+}
