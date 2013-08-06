8de1917d142de0fb20e42e7f18eabe558af2e2c2
Maximilian Schneider-Ludorff <msl@tapefabrik.de>
2013-08-06 14:48:06 +0200

    Beginn der Notifications.

--- app/controller/helpers/EmailOutbound.php
+++ app/controller/helpers/EmailOutbound.php
@@ -9,8 +9,50 @@ class EmailOutbound {
 	 * @param Order $order
 	 */
 	static function sendNotification($order){
-		// get new status from order
 		
-		// do fancy things
+		// Check which mail is to be send.
+		print_r($order);
+
 	}
+
+	static function sendBilling($order){
+		$message = "Hallo ".VORNAME.",
+
+					Vielen Dank für deine Ticketbestellung !
+
+					Im Folgenden deine Rechnung und die Überweisungsdaten, sobald das Geld bei uns eingeht, schicken wir dir deine Tickets. Bitte überweise möglichst bald, denn wir können aufgrund der großen Nachfrage keine Tickets reservieren. Nur  was bis Montag bei uns eingeht, kann berücksichtigt werden.
+
+					Rechnungs-/Kundennummer: ".RECHNUNGSNUMMER."
+
+					#ANZAHL# x VVK-Ticket - Tapefabrik #3 mit Die Bestesten, Mach One u.a. - 21,- €
+					1 x Pauschale für Verpackung und Versand - 1,- €
+					------------------------------------------------------------------------------------------
+					Gesamtbetrag: #GESAMTBETRAG#,- €
+
+					Bitte überweise den Gesamtbetrag mit Folgenden Daten sobald wie möglich:
+
+					Inhaber: Maximilian Schneider-Ludorff
+					KontoNr: 535370324
+					BLZ: 510 500 15
+					Kreditinstitut: Naspa Limburg
+					Verwendungszweck: Ticket - <DEINE Rechnungsnummer>
+
+					Viele Grüße, wir freuen uns auf dich !
+
+					Maximilian Schneider-Ludorff
+					-------------------------------------------------------
+					TAPEFABRIK // Logistik
+
+					E-Mail: tickets@tapefabrik.de
+					Web: http://www.tapefabrik.de";
+	}
+
+	static function sendNotificationMail($adress, $subject, $message){
+		$header = 'From: '.SHOPADRESS. "\r\n" .
+		    'Reply-To: ' .SUPPORTADRESS. "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
+
+		mail($adress, $subject, $message, $header);
+	}
+
 }
