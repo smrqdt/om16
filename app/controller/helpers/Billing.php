5269ad779bcecc9189e97e57c9a415a7e854c9ee
Maximilian Schneider-Ludorff <msl@tapefabrik.de>
2013-08-25 23:01:48 +0200

    Editor

---
+++ app/controller/helpers/Billing.php
@@ -0,0 +1,63 @@
+<?php
+
+class Billing extends FPDF {
+
+	function Meta(){
+		$this->AddFont('Sanchez','','sanchez.php');
+	}
+	
+	function Body(){
+
+		$this->Ln(37);
+  		$this->Cell(16.5);
+
+		$this->SetTextColor(0,0,0);
+		$message = 
+"Hallo <<Vorname>>,\n
+\n
+wir möchten uns bei dir noch einmal für deine Bestellung bedanken.\n
+\n
+Nach dem ersten Event in Limburg, versuchen wir auch in Wiesbaden, den freshesten Rap\n
+Deutschlands auf die Bühnen zu stellen - ohne dich wär das nicht möglich !\n
+\n
+Wir freuen uns sehr auf deinen Besuch, alle Informationen die du brauchst findest du weiterhin\n
+unter:\n
+\n
+      - tapefabrik.de\n
+      - facebook.com/tapefabrik\n
+\n
+Wenn du möchtest lade doch bei Facebook noch deine Leute ein, uns hilft jeder Besucher !\n
+\n
+Hier deine <<Anzahl>> Tickets mit der Rechnungsnummer <<Rechnungsnummer>>.\n
+\n
+Bis zum 25. Januar - Dein Tapefabrik Team !\n";
+		$this->MultiCell(0,2.2,utf8_decode($message));
+	}
+
+
+	function Header(){
+		$this->Meta();
+
+		// Print the Header
+		$this->SetFont('Sanchez','',10);
+		$this->Image('Ticketbestellungen.png',0,0,-150,-150);
+  		$this->SetTextColor(255,255,255);
+
+  		$this->Ln(49.5);
+  		$this->Cell(16.5);
+
+		$this->Cell(472,3,utf8_decode("<<Vorname>> <<Name>>"));
+		$this->Ln(6);
+
+  		$this->Cell(16.5);
+		$this->Cell(472,3,utf8_decode("<<Straße>> <<Hausnummer>>"));
+		$this->Ln(6);
+
+  		$this->Cell(16.5);
+		$this->Cell(472,3,utf8_decode("<<PLZ>> <<Ort>>"));
+
+		$this->Body();
+	}
+}
+
+?>
