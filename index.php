48280e08dcf9f46c486371840a370867c5f9f7ab
Robert Krause <rokr42@gmail.com>
2014-10-11 18:58:08 +0200

    fix #64

--- index.php
+++ index.php
@@ -61,54 +61,54 @@ $app->map('/signup/', array($loginController, 'signup'))->via('GET', 'POST')->na
 // Shop
 $shopController = new Tapeshop\Controllers\ShopController();
 $app->get('/', array($shopController, 'index'))->name('home');
-$app->get('/checkout', array($shopController, 'checkout'))->name("checkout");
-$app->post('/noSignup', array($shopController, "noSignup"));
-$app->get('/ticketscript', array($shopController, "ticketscript"));
+$app->get('/checkout/', array($shopController, 'checkout'))->name("checkout");
+$app->post('/noSignup/', array($shopController, "noSignup"));
+$app->get('/ticketscript/', array($shopController, "ticketscript"));
 
 // Admin routings
 $adminController = new Tapeshop\Controllers\AdminController();
-$app->get('/admin', array($adminController, 'index'))->name('admin');
-$app->get('/admin/items', array($adminController, 'items'))->name('adminitems');
-$app->get('/admin/orders', array($adminController, 'orders'))->name('adminorders');
+$app->get('/admin/', array($adminController, 'index'))->name('admin');
+$app->get('/admin/items/', array($adminController, 'items'))->name('adminitems');
+$app->get('/admin/orders/', array($adminController, 'orders'))->name('adminorders');
 
 // order routings
 $orderController = new \Tapeshop\Controllers\OrderController();
 $orderController::updateStatus();
-$app->post('/order', array($orderController, 'submitOrder'));
-$app->get('/order/:hash', array($orderController, "order"))->name("order");
-$app->post('/order/delete/:id', array($orderController, 'deleteOrder'));
-$app->post('/order/:id/payed', array($orderController, 'payed'));
-$app->get('/order/billing/:hash', array($orderController, 'billing'));
-$app->post('/order/:id/shipped', array($orderController, 'shipped'));
+$app->post('/order/', array($orderController, 'submitOrder'));
+$app->get('/order/:hash/', array($orderController, "order"))->name("order");
+$app->post('/order/delete/:id/', array($orderController, 'deleteOrder'));
+$app->post('/order/:id/payed/', array($orderController, 'payed'));
+$app->get('/order/billing/:hash/', array($orderController, 'billing'));
+$app->post('/order/:id/shipped/', array($orderController, 'shipped'));
 
 // cart routings
 $cartController = new \Tapeshop\Controllers\CartController();
-$app->post('/cart/addItem/:id', array($cartController, 'addItem'));
-$app->get('/cart', array($cartController, 'cart'))->name('cart');
-$app->post('/cart/clear', array($cartController, 'clearCart'));
-$app->post('/cart/increase', array($cartController, 'increase'));
-$app->post('/cart/decrease', array($cartController, 'decrease'));
-$app->post('/cart/remove', array($cartController, 'remove'));
-$app->post('/cart/changesize', array($cartController, 'changeSize'));
+$app->post('/cart/addItem/:id/', array($cartController, 'addItem'));
+$app->get('/cart/', array($cartController, 'cart'))->name('cart');
+$app->post('/cart/clear/', array($cartController, 'clearCart'));
+$app->post('/cart/increase/', array($cartController, 'increase'));
+$app->post('/cart/decrease/', array($cartController, 'decrease'));
+$app->post('/cart/remove/', array($cartController, 'remove'));
+$app->post('/cart/changesize/', array($cartController, 'changeSize'));
 
 // user routings
 $userController = new \Tapeshop\Controllers\UserController();
-$app->post('/user/delete/:id', array($userController, 'delete'));
-$app->get('/admin/user/edit/:id', array($userController, 'edit'));
-$app->map('/admin/user/save/:id', array($userController, 'save'))->via('GET', 'POST')->name('editUser');
+$app->post('/user/delete/:id/', array($userController, 'delete'));
+$app->get('/admin/user/edit/:id/', array($userController, 'edit'));
+$app->map('/admin/user/save/:id/', array($userController, 'save'))->via('GET', 'POST')->name('editUser');
 
 // item routings
 $itemController = new \Tapeshop\Controllers\ItemController();
-$app->get('/item/:id', array($itemController, 'show'));
-$app->map('/item/edit/:id', array($itemController, 'edit'))->via('GET', 'POST')->name('editItem');
-$app->post('/item/delete/:id', array($itemController, 'delete'));
-$app->post('/item/:id/addsize', array($itemController, 'addSize'));
-$app->post('/item/:id/removeimage', array($itemController, 'removeImage'));
-$app->post('/item/:id/addnumbers', array($itemController, 'addNumbers'));
-$app->post('/item/:id/takenumbers', array($itemController, 'takeNumbers'));
-$app->post('/item/:id/invalidatenumbers', array($itemController, 'invalidateNumbers'));
+$app->get('/item/:id/', array($itemController, 'show'));
+$app->map('/item/edit/:id/', array($itemController, 'edit'))->via('GET', 'POST')->name('editItem');
+$app->post('/item/delete/:id/', array($itemController, 'delete'));
+$app->post('/item/:id/addsize/', array($itemController, 'addSize'));
+$app->post('/item/:id/removeimage/', array($itemController, 'removeImage'));
+$app->post('/item/:id/addnumbers/', array($itemController, 'addNumbers'));
+$app->post('/item/:id/takenumbers/', array($itemController, 'takeNumbers'));
+$app->post('/item/:id/invalidatenumbers/', array($itemController, 'invalidateNumbers'));
 $app->post('/item/:id/makenumbered', array($itemController, 'makeNumbered'));
-$app->post('/item/deletesize/:id', array($itemController, 'deleteSize'));
-$app->map('/items/create', array($itemController, 'create'))->via('GET', 'POST');
+$app->post('/item/deletesize/:id/', array($itemController, 'deleteSize'));
+$app->map('/items/create/', array($itemController, 'create'))->via('GET', 'POST');
 
 $app->run();
