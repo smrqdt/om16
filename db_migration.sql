081e76a70caa329b67596ffde1148df140c46f62
Robert Krause <rokr42@gmail.com>
2014-10-23 18:27:22 +0200

    Added payment methods to db_migration script

--- db_migration.sql
+++ db_migration.sql
@@ -23,3 +23,31 @@ ALTER TABLE  `sizes` ADD  `deleted` BOOLEAN NOT NULL DEFAULT FALSE ;
 ALTER TABLE  `itemnumbers` DROP  `free` ;
 
 ALTER TABLE  `items` ADD  `shownumbers` BOOLEAN NOT NULL DEFAULT TRUE ;
+
+CREATE TABLE IF NOT EXISTS `payment_methods` (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `fix` int(5) NOT NULL,
+  `fee` float(5,3) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Daten für Tabelle `payment_methods`
+--
+
+INSERT INTO `payment_methods` (`id`, `name`, `fix`, `fee`) VALUES
+  (1, 'paypal', 35, 0.019),
+  (2, 'sofort', 25, 0.009),
+  (3, 'prepay', 0, 0.000);
+
+ALTER TABLE `orders` ADD `payment_method_id` INT NULL;
+
+ALTER TABLE  `orders` ADD FOREIGN KEY (  `payment_method_id` ) REFERENCES  `tapeshop`.`payment_methods` (
+  `id`
+) ON DELETE RESTRICT ON UPDATE RESTRICT ;
+
+ALTER TABLE `orders` ADD `payment_id` VARCHAR(64) NULL;
+ALTER TABLE `orders` ADD `payment_fee` INT NOT NULL DEFAULT '0';
+ALTER TABLE `orders` ADD `payment_status` VARCHAR(32) NULL;
