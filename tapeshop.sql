d99d32266608ff82de44c6356a2976cf9a9d2be5
Robert Krause <rokr42@gmail.com>
2014-10-23 17:36:25 +0200

    Updated SQL schema to

--- tapeshop.sql
+++ tapeshop.sql
@@ -1,34 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.0.3
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Erstellungszeit: 30. Sep 2014 um 23:01
--- Server Version: 5.6.17
--- PHP-Version: 5.4.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Datenbank: `tapeshop`
---
 CREATE DATABASE IF NOT EXISTS `tapeshop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `tapeshop`;
+USE tapeshop;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `addresses`
---
-
-CREATE TABLE IF NOT EXISTS `addresses` (
+CREATE TABLE `addresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `name` varchar(512) NOT NULL,
@@ -41,34 +15,22 @@ CREATE TABLE IF NOT EXISTS `addresses` (
   `current` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=85 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `itemnumbers`
---
-
-CREATE TABLE IF NOT EXISTS `itemnumbers` (
+CREATE TABLE `itemnumbers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `orderitem_id` int(11) DEFAULT NULL,
   `number` int(11) NOT NULL,
   `valid` tinyint(1) NOT NULL DEFAULT '1',
-  `free` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  KEY `item_id` (`item_id`,`orderitem_id`),
+  KEY `item_id` (`item_id`),
   KEY `orderitem_id` (`orderitem_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1570 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `items`
---
-
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `manage_stock` tinyint(1) NOT NULL DEFAULT '0',
   `name` varchar(128) DEFAULT NULL,
   `description` varchar(512) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
@@ -77,34 +39,25 @@ CREATE TABLE IF NOT EXISTS `items` (
   `shipping` int(11) NOT NULL,
   `ticketscript` tinyint(1) DEFAULT '0',
   `deleted` tinyint(1) DEFAULT '0',
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `shownumbers` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `orderitems`
---
-
-CREATE TABLE IF NOT EXISTS `orderitems` (
+CREATE TABLE `orderitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
-  `size` varchar(32) DEFAULT NULL,
   `price` int(11) NOT NULL,
+  `size_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `item` (`item_id`),
-  KEY `order` (`order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=132 ;
+  KEY `order` (`order_id`),
+  KEY `size_id` (`size_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `orders`
---
-
-CREATE TABLE IF NOT EXISTS `orders` (
+CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `ordertime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -121,88 +74,50 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`id`),
   KEY `user` (`user_id`),
   KEY `hashlink` (`hashlink`),
-  KEY `hashlink_2` (`hashlink`),
-  KEY `hashlink_3` (`hashlink`),
-  KEY `hashlink_4` (`hashlink`),
-  KEY `hashlink_5` (`hashlink`),
   KEY `address_id` (`address_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=171 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `payment_methods`
---
-
-CREATE TABLE IF NOT EXISTS `payment_methods` (
+CREATE TABLE `payment_methods` (
   `id` int(2) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) NOT NULL,
   `fix` int(5) NOT NULL,
   `fee` float(5,3) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `sizes`
---
-
-CREATE TABLE IF NOT EXISTS `sizes` (
+CREATE TABLE `sizes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `size` varchar(64) NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `item` (`item_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=68 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Constraints der exportierten Tabellen
---
 
---
--- Constraints der Tabelle `itemnumbers`
---
 ALTER TABLE `itemnumbers`
-  ADD CONSTRAINT `itemnumbers_ibfk_1` FOREIGN KEY (`orderitem_id`) REFERENCES `orderitems` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `itemnumbers_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON UPDATE CASCADE;
+ADD CONSTRAINT `itemnumbers_ibfk_1` FOREIGN KEY (`orderitem_id`) REFERENCES `orderitems` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `itemnumbers_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON UPDATE CASCADE;
 
---
--- Constraints der Tabelle `orderitems`
---
 ALTER TABLE `orderitems`
-  ADD CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `orderitems_ibfk_3` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`id`);
 
---
--- Constraints der Tabelle `orders`
---
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`);
+ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`);
 
---
--- Constraints der Tabelle `sizes`
---
 ALTER TABLE `sizes`
-  ADD CONSTRAINT `sizes_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ADD CONSTRAINT `sizes_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
